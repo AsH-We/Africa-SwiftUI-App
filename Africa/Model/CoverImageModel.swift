7c35f48027cf06facd3f597d9f9648a2545feba2
AsH-We <abilmansur.ashim@gmail.com>
2023-02-10 15:50:35 +0600

    Second Part of the project done I wrote the extension to decode JSON added CoverImageModel and eventually done view for covers

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Abilmansur Ashim on 10.02.2023.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
