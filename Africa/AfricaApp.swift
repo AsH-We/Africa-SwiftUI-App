2d3d7c0b2edba248c93e86117f23988426444467
AsH-We <abilmansur.ashim@gmail.com>
2023-02-10 10:27:17 +0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Abilmansur Ashim on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
