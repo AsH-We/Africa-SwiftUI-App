5483444d57d9fca9285aa8d4c27a4c4412148286
AsH-We <abilmansur.ashim@gmail.com>
2023-02-12 19:53:05 +0600

    Fourth part of the project is done. There i realized detail view for animals

---
+++ Africa/View/HeadingView.swift
@@ -0,0 +1,40 @@
+//
+//  headingView.swift
+//  Africa
+//
+//  Created by Abilmansur Ashim on 12.02.2023.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    // MARK: - PROPERTIES
+    
+    var headingImage: String
+    var headingText: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Image(systemName: headingImage)
+                .foregroundColor(.accentColor)
+                .imageScale(.large)
+            
+            Text(headingText)
+                .font(.title3)
+                .fontWeight(.bold)
+        } //: HSTACK
+        .padding(.vertical)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct HeadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
